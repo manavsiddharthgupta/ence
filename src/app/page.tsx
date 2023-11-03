dd4e9e35a3f44cad02ddfa781996002dbdaf7ae6
Manav Gupta <2006468@kiit.ac.in>
2023-11-03 12:44:52 +0530

    [FIX] used 'permanentRedirect' to redirect '/' to '/home'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,4 @@
-"use client";
-import React, { useEffect } from "react";
-import { useRouter } from "next/navigation";
-
+import { permanentRedirect } from "next/navigation";
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    router.replace("/home");
-  }, []);
-
-  return (
-    <div>
-      <p>Welcome to Ence 🚀 </p>
-    </div>
-  );
+  permanentRedirect("/home");
 }
