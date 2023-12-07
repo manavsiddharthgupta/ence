7b2373fa75c54134427851bf28dc2208e7c04e9b
Manav Gupta <2006468@kiit.ac.in>
2023-12-07 23:45:19 +0530

    [ADDED] 'invoices/overview' api + minor ui change

--- src/app/invoice/lists/page.tsx
+++ src/app/invoice/lists/page.tsx
@@ -1,8 +1,5 @@
-import {
-  ArrowDownRightFromCircleIcon,
-  ArrowUpRightFromCircleIcon
-} from 'lucide-react'
 import InvoiceTable from './invoices-table'
+import Overview from './overview'
 
 const Lists = () => {
   // will have to revamp code
@@ -12,90 +9,10 @@ const Lists = () => {
         Overview
       </p>
       <h1 className='text-4xl leading-9 font-semibold'>Invoice Lists</h1>
-      <div className='my-6 w-full border-[1.5px] dark:border-zinc-700/60 border-zinc-300/60 rounded-lg bg-zinc-300/20 dark:bg-zinc-600/5 backdrop-blur-xl py-6 px-8 flex justify-between'>
-        <InvoiceTypeOverview
-          count={5}
-          extraCount={1}
-          isIncreased={false}
-          rate={-2.4}
-          type='Paid'
-        />
-        <InvoiceTypeOverview
-          count={2}
-          extraCount={4}
-          isIncreased={true}
-          rate={1.4}
-          type='Unpaid'
-        />
-        <InvoiceTypeOverview
-          count={1}
-          extraCount={3}
-          isIncreased={true}
-          rate={7.8}
-          type='Pending'
-        />
-        <InvoiceTypeOverview
-          count={3}
-          extraCount={1}
-          isIncreased={false}
-          rate={-1.2}
-          type='Partially Paid'
-          isLast={true}
-        />
-      </div>
+      <Overview />
       <InvoiceTable />
     </div>
   )
 }
 
 export default Lists
-
-const InvoiceTypeOverview = ({
-  type,
-  count,
-  extraCount,
-  rate,
-  isIncreased,
-  isLast = false
-}: {
-  type: string
-  rate: number
-  count: number
-  extraCount: number
-  isIncreased: Boolean
-  isLast?: Boolean
-}) => {
-  return (
-    <div
-      className={
-        isLast === false
-          ? 'border-r-2 border-zinc-300 dark:border-zinc-700 pr-20'
-          : 'pr-12'
-      }
-    >
-      <p className='text-xs text-zinc-700 dark:text-zinc-400 font-medium'>
-        {type} ({extraCount})
-      </p>
-      <h1 className='text-2xl font-bold pl-1'>{count}</h1>
-      <div className='flex gap-1 items-center'>
-        <span className='text-[10px] dark:text-zinc-400/70 text-zinc-700/70'>
-          vs last week
-        </span>
-        <span className='text-xs font-semibold'>{rate}%</span>
-        {isIncreased === true ? (
-          <ArrowUpRightFromCircleIcon
-            size='12px'
-            color='green'
-            strokeWidth='3px'
-          />
-        ) : (
-          <ArrowDownRightFromCircleIcon
-            size='12px'
-            color='red'
-            strokeWidth='3px'
-          />
-        )}
-      </div>
-    </div>
-  )
-}
