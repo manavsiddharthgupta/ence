e6778bb8fe05705fb24c18f7e31b7c22e957825e
Manav Gupta <2006468@kiit.ac.in>
2023-12-30 23:36:52 +0530

    [ADDED] Add authentication and error handling to POST route

--- src/app/api/instant/invoice/route.ts
+++ src/app/api/instant/invoice/route.ts
@@ -1,6 +1,42 @@
-export async function POST() {
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../../auth/[...nextauth]/route'
+import { db } from '@/lib/db'
+
+export async function POST(request: Request) {
   try {
-  } catch (error) {}
+    const session = await getServerSession(authOptions)
+    const email = session?.user?.email
+
+    if (!email) {
+      console.error('Error:', 'Not Authorized')
+      return Response.json({ ok: false, data: null, status: 401 })
+    }
+
+    const org = await db.user.findUnique({
+      where: {
+        email: email
+      },
+      select: {
+        email: true,
+        organizations: {
+          select: {
+            id: true,
+            orgName: true
+          }
+        }
+      }
+    })
+
+    if (!org?.organizations?.id) {
+      console.error('Error:', 'Organization Not Found')
+      return Response.json({ ok: false, data: null, status: 404 })
+    }
+
+    const blob = await request.blob()
+  } catch (error) {
+    console.error('Error:', error)
+    return Response.json({ ok: false, data: null, status: 500 })
+  }
 }
 
 // user -> image -> S3 -> link -> ocr_service -> data_res -> validation -> data_return
