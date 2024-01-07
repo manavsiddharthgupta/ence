310d3edae64838749189bd720eae3b1adc6c2d80
Manav Gupta <2006468@kiit.ac.in>
2024-01-07 19:15:44 +0530

    [FIX] fetch used in server component

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,12 @@
 export { default } from 'next-auth/middleware'
+
+export const config = {
+  matcher: [
+    '/invoice/:path*',
+    '/',
+    '/onboarding',
+    '/instant/:path*',
+    '/home',
+    '/api:path*'
+  ]
+}
