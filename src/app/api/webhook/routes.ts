0897f0e2e980edb78ee960bcf878f7e5ff8509d3
Manav Gupta <2006468@kiit.ac.in>
2024-01-23 03:24:56 +0530

    [WIP] whatsapp integration

--- src/app/api/webhook/routes.ts
+++
@@ -1,6 +0,0 @@
-import whatsappWebhookModule from '@/webhooks/whatsapp'
-import { NextApiRequest } from 'next'
-
-export async function POST(request: NextApiRequest) {
-  whatsappWebhookModule(request)
-}
