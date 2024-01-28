149af6e7a1cb7d969e026a01faca7b6314d5fb8a
Manav Gupta <2006468@kiit.ac.in>
2024-01-28 16:26:24 +0530

    Update runtime value in worker route

--- src/app/api/worker/route.ts
+++ src/app/api/worker/route.ts
@@ -2,7 +2,7 @@ import { sqsConsumerConfig } from '@/config/worker'
 import { SqsConsumer } from '@/sqs/resources/sqsConsumer'
 import { SQSProcessor } from '@/sqs/resources/sqsProcessor'
 
-export const runtime = 'node'
+export const runtime = 'nodejs'
 
 export async function GET() {
   const sqsConsumer = new SqsConsumer({
