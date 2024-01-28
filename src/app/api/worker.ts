7f22c5db6e081c520b85dd0e93093246fbf2257e
Manav Gupta <2006468@kiit.ac.in>
2024-01-28 16:16:10 +0530

    Add runtime constant to worker.ts

--- src/app/api/worker.ts
+++ src/app/api/worker.ts
@@ -2,6 +2,8 @@ import { sqsConsumerConfig } from '@/config/worker'
 import { SqsConsumer } from '@/sqs/resources/sqsConsumer'
 import { SQSProcessor } from '@/sqs/resources/sqsProcessor'
 
+export const runtime = 'node'
+
 export async function GET() {
   const sqsConsumer = new SqsConsumer({
     name: 'worker',
