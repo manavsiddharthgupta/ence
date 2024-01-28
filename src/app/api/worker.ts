09aad687c2e62f3c6e0d5ff9b6aab459388e6ae7
Manav Gupta <2006468@kiit.ac.in>
2024-01-28 16:14:53 +0530

    Refactor sqsConsumer initialization in worker.ts

--- src/app/api/worker.ts
+++ src/app/api/worker.ts
@@ -2,12 +2,14 @@ import { sqsConsumerConfig } from '@/config/worker'
 import { SqsConsumer } from '@/sqs/resources/sqsConsumer'
 import { SQSProcessor } from '@/sqs/resources/sqsProcessor'
 
-export const sqsConsumer = new SqsConsumer({
-  name: 'worker',
-  ...sqsConsumerConfig,
-  handleMessage: SQSProcessor.handleMessage
-})
-console.log('SQS CONSUMER started')
-sqsConsumer.start().catch((error) => {
-  console.error('Failed to start Consumer', error)
-})
+export async function GET() {
+  const sqsConsumer = new SqsConsumer({
+    name: 'worker',
+    ...sqsConsumerConfig,
+    handleMessage: SQSProcessor.handleMessage
+  })
+  console.log('SQS CONSUMER started')
+  sqsConsumer.start().catch((error) => {
+    console.error('Failed to start Consumer', error)
+  })
+}
