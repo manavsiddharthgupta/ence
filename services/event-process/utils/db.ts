119e20fc89f30ca15571669b16ef691adb57e870
devansh dwivedi <devanshdwivedi68@gmail.com>
2024-02-09 00:39:16 +0530

    [UPDATE]: serverless updated 38

--- services/event-process/utils/db.ts
+++ services/event-process/utils/db.ts
@@ -1,5 +1,4 @@
-// import { PrismaClient } from '../db/src/index'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from 'database'
 
 declare global {
   // eslint-disable-next-line no-var, no-unused-vars
