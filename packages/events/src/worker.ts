7516b04034ed61c5f0cf55c8501df2168cbd45c6
Manav Gupta <2006468@kiit.ac.in>
2024-02-14 17:34:52 +0530

    fix: invalid url, host is blacklisted

--- packages/events/src/worker.ts
+++ packages/events/src/worker.ts
@@ -2,22 +2,23 @@ import { Client } from '@upstash/qstash'
 
 const q = new Client({
   token: process.env.QSTASH_TOKEN!,
-  retry: false // will change later
+  retry: {
+    retries: 3
+  }
 })
 
-const url =
-  process.env.QSTASH_URL! +
-  process.env.NEXT_PUBLIC_API_URL! +
-  '/api/events/invoice'
 export class WorkerQueue {
   static async push(value: any) {
     console.log('Job to be pushed to the Q_STASH', value)
     try {
       const result = await q.publish({
-        url: url,
-        body: JSON.stringify(value)
+        url: process.env.NEXT_PUBLIC_API_URL! + '/api/events/invoice',
+        body: JSON.stringify(value),
+        headers: {
+          Authorization: `Bearer ${process.env.QSTASH_TOKEN!}`,
+          'Content-Type': 'application/json'
+        }
       })
-
       console.log('Message sent successfully:', result)
       return result
     } catch (error) {
