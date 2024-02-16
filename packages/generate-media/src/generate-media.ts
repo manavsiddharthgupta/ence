ca49a61838cfb73113415bba915f59ccb6378cf8
Manav Gupta <2006468@kiit.ac.in>
2024-02-16 22:24:44 +0530

    fix: puppeteer could not find Chrome (ver. 114.0.5735.90)

--- packages/generate-media/src/generate-media.ts
+++ packages/generate-media/src/generate-media.ts
@@ -29,7 +29,6 @@ export const generateMedia = async (
 ) => {
   const browser = await puppeteer.launch()
   const page = await browser.newPage()
-  await page.setCacheEnabled(false)
 
   const content = await compile(
     `src/templates/${category}/${templateName}.hbs`,
