f715c592edeb9398179e2cce2658d817bfb27a10
Manav Gupta <2006468@kiit.ac.in>
2024-02-17 12:16:00 +0530

    test 12: update executable path

--- packages/generate-media/src/generate-media.ts
+++ packages/generate-media/src/generate-media.ts
@@ -28,7 +28,13 @@ export const generateMedia = async (
   type: 'PDF' | 'IMAGE'
 ) => {
   const browser = await chromium.launch({
-    headless: true
+    executablePath: '/usr/bin/chromium-browser',
+    args: [
+      '--disable-gpu',
+      '--disable-setuid-sandbox',
+      '--no-sandbox',
+      '--no-zygote'
+    ]
   })
   const page = await browser.newPage()
 
