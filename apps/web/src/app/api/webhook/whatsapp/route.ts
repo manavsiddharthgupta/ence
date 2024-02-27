23594535993a57b192fbad86ec5b931bbf50f016
Manav Gupta <2006468@kiit.ac.in>
2024-02-28 03:15:23 +0530

    ref: rmv db calls from whatsapp callback-url

--- apps/web/src/app/api/webhook/whatsapp/route.ts
+++ apps/web/src/app/api/webhook/whatsapp/route.ts
@@ -1,6 +1,3 @@
-import { db } from '@/lib/db'
-import { InvoiceApprovalStatus } from '@/types/invoice'
-
 export async function GET(request: Request) {
   const verify_token = process.env.WHATSAPP_VERIFICATION_TOKEN
 
@@ -21,111 +18,19 @@ export async function GET(request: Request) {
 // Todo:
 export async function POST(request: Request) {
   const whatsapp_token = process.env.WHATSAPP_TOKEN
-
-  const requestBody = await request.json()
-
-  if (requestBody?.object) {
-    if (
-      requestBody.entry &&
-      requestBody.entry[0].changes &&
-      requestBody.entry[0].changes[0] &&
-      requestBody.entry[0].changes[0].value.messages &&
-      requestBody.entry[0].changes[0].value.messages[0]
-    ) {
-      let phone_number_id =
-        requestBody.entry[0].changes[0].value.metadata.phone_number_id
-      let from = requestBody.entry[0].changes[0].value.messages[0].from
-      let msg_body =
-        requestBody.entry[0].changes[0].value.messages[0].button.text
-
-      let status: InvoiceApprovalStatus =
-        msg_body === 'Approve'
-          ? InvoiceApprovalStatus.APPROVED
-          : InvoiceApprovalStatus.REJECTED
-
-      const invoiceId = requestBody.invoiceId
-
-      if (msg_body === 'Approve') {
-        console.log('---> in approve')
-        fetch(
-          'https://graph.facebook.com/v12.0/' +
-            phone_number_id +
-            '/messages?access_token=' +
-            whatsapp_token,
-          {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              messaging_product: 'whatsapp',
-              to: from,
-              text: {
-                body: 'https://ence.in'
-              }
-            })
-          }
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            console.log('Approval response:', data)
-          })
-          .catch((error) => {
-            console.error('Approval error:', error)
-          })
-
-        await db.invoice.update({
-          where: {
-            id: invoiceId
-          },
-          data: {
-            approvalStatus: status
-          },
-          select: {
-            invoiceNumber: true,
-            approvalStatus: true
-          }
-        })
-      } else if (msg_body === 'Reject') {
-        fetch(
-          'https://graph.facebook.com/v12.0/' +
-            phone_number_id +
-            '/messages?access_token=' +
-            whatsapp_token,
-          {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-              messaging_product: 'whatsapp',
-              to: from,
-              text: {
-                body: 'Sorry! We are improving our services each day.'
-              }
-            })
-          }
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            console.log('Rejection response:', data)
-          })
-          .catch((error) => {
-            console.error('Rejection error:', error)
-          })
-
-        await db.invoice.update({
-          where: {
-            id: invoiceId
-          },
-          data: {
-            approvalStatus: status
-          },
-          select: {
-            invoiceNumber: true,
-            approvalStatus: true
-          }
-        })
-      }
-    }
-    return new Response('Success', { status: 200 })
-  } else {
-    return new Response('Not found', { status: 404 })
+  // do something
+  try {
+    const requestBody = await request.json()
+    return Response.json({
+      ok: true,
+      data: requestBody,
+      status: 200
+    })
+  } catch (err) {
+    return Response.json({
+      ok: false,
+      data: null,
+      status: 500
+    })
   }
 }
--- packages/events/src/processor/invoice-jobs.ts
+++ packages/events/src/processor/invoice-jobs.ts
@@ -1,10 +1,8 @@
 import { sendInvoiceThroughMail } from 'send-media'
-import { whatsappService } from '../../../../apps/web/src/integration/whatsapp/service'
 export class InvoiceJobsProcessor {
   static async processInvoiceData(body: any) {
     console.log('processing started...')
     const invoiceData = body?.data
-    const invoiceId = body?.invoiceId
     const email = body?.data?.customerInfo?.email
     if (!email || !invoiceData) {
       console.error('Invalid payload')
@@ -12,6 +10,5 @@ export class InvoiceJobsProcessor {
     }
     await sendInvoiceThroughMail(email, invoiceData)
     // sending through whatsapp
-    await whatsappService(invoiceData.customerInfo.name, invoiceData, invoiceId)
   }
 }
